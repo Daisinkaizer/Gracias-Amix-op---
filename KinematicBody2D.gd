9068c7a784b5ab420555e6dd173907c99c1ee637
Daisinkaizer <ivan.a.mansilla@hotmail.com>
2019-05-02 14:28:21 -0300

    Initial commit

---
+++ KinematicBody2D.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+var velocidad = Vector2(0,0)
+
+var gravedad = 10;
+
+
+
+func _physics_process(delta):
+	
+	if velocidad.y < 100:
+		velocidad.y += gravedad;
+	
+	if Input.is_action_pressed("ui_right")and velocidad.x < 200:
+		velocidad.x += 200;
+	if Input.is_action_pressed("ui_left") and velocidad.x > -200:
+		velocidad.x -= 20;
+	if Input.is_action_just_released("ui_right"):
+		velocidad.x = 0;
+	if Input.is_action_just_released("ui_left"):
+		velocidad.x = 0;
+	
+	
+	#if Input.is_action_pressed("ui_up")and velocidad.x < 200:
+		#velocidad.y -= 20;
+	#if Input.is_action_pressed("ui_down") and velocidad.x > -200:
+		#velocidad.y += 20;
+	#if Input.is_action_just_released("ui_up"):
+		#velocidad.y = 0;
+	#if Input.is_action_just_released("ui_down"):
+		#velocidad.y = 0;
+	if Input.is_action_just_pressed("ui_up"):
+		velocidad.y = -200
+
+	move_and_slide(velocidad)
