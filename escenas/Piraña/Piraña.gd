570e13927891e9e4d33f2c0935a1092427a42c5b
Tatomon1 <tatoraton3@outlook.com>
2019-05-05 17:36:55 -0300

    Movimiento enemigo
    Modifique el movimiento del enemigo tiburon, aun no puedo hacer funcionar a la piraña, pero es cuestion de tiempo.

---
+++ escenas/Piraña/Piraña.gd
@@ -0,0 +1,19 @@
+extends AnimatedSprite
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _process(delta):
+	position.y = 5
+	pass
+
+
+func _on_Area2D_area_entered(area):
+	queue_free()
+	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	pass # Replace with function body.
