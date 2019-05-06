8bf16524f5333c5d0acaad531c59c91c681fc1e8
Tatomon1 <tatoraton3@outlook.com>
2019-05-06 16:50:09 -0300

    Agregue bordes arriba y abajo de la pantalla
    Solo puse el tope de arriba y abajo, me falta agregar el de los costados.

--- escenas/Personaje/KinematicBody2D.gd
+++ escenas/Personaje/KinematicBody2D.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 var velocidad = Vector2(0,0)
-
+var screen_size;
 var gravedad = 15;
+var vida = 3
 
-
+func _ready():
+    screen_size = get_viewport_rect().size
 
 func _physics_process(delta):
 	
@@ -18,17 +20,14 @@ func _physics_process(delta):
 		velocidad.x = 0;
 	if Input.is_action_just_released("ui_left"):
 		velocidad.x = 0;
-	
-	
-	#if Input.is_action_pressed("ui_up")and velocidad.x < 200:
-		#velocidad.y -= 20;
-	#if Input.is_action_pressed("ui_down") and velocidad.x > -200:
-		#velocidad.y += 20;
-	#if Input.is_action_just_released("ui_up"):
-		#velocidad.y = 0;
-	#if Input.is_action_just_released("ui_down"):
-		#velocidad.y = 0;
 	if Input.is_action_just_pressed("ui_up"):
 		velocidad.y = -200
-
+		position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	pass
 	move_and_slide(velocidad)
+
+func _on_Area2D_area_entered(area):
+	vida -= 1
+	pass # Replace with function body.
+
